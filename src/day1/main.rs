4f1f79bfc504397efb3a75fb9500cfbcbf183a0e
Wendell Smith <wackywendell@gmail.com>
2020-01-25 22:03:57 -0500

    Added basic day 1

---
+++ src/day1/main.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+use clap::{App, Arg};
+use log::debug;
+
+fn main() -> Result<(), failure::Error> {
+    env_logger::init();
+
+    let matches = App::new("Day 24")
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("INPUT")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let input_path = matches.value_of("INPUT").unwrap_or("inputs/day24.txt");
+
+    debug!("Using input {}", input_path);
+    let file = File::open(input_path)?;
+    let buf_reader = BufReader::new(file);
+
+    for line in buf_reader.lines() {
+        println!("{}", line?)
+    }
+
+    Ok(())
+}
