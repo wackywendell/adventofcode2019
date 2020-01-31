4395414f4cd697854c05cad235b42209dd75c657
Wendell Smith <wackywendell@gmail.com>
2020-01-31 16:56:21 -0500

    Day 4 works

--- src/day4/main.rs
+++ src/day4/main.rs
@@ -45,6 +45,44 @@ impl Password {
 
         repeats
     }
+
+    pub fn extra_valid(&self) -> bool {
+        log::debug!("Testing validity of {:?}", self.digits);
+        if self.digits.len() != 6 {
+            log::debug!("  FAIL: Length {} != 6", self.digits.len());
+            return false;
+        }
+
+        let mut rep_count = 1;
+        let mut doublet = false;
+        for (l, n) in self.digits.iter().zip(&self.digits[1..]) {
+            if n < l {
+                return false;
+            }
+            match n.cmp(l) {
+                std::cmp::Ordering::Less => {
+                    log::debug!("  FAIL: Out of order {} < {}", n, l);
+                    return false;
+                }
+                std::cmp::Ordering::Equal => rep_count += 1,
+                std::cmp::Ordering::Greater if rep_count == 2 => {
+                    doublet = true;
+                    rep_count = 1;
+                }
+                std::cmp::Ordering::Greater => {
+                    rep_count = 1;
+                }
+            }
+        }
+
+        doublet |= rep_count == 2;
+
+        if !doublet {
+            log::debug!("  FAIL: No doublet");
+        }
+
+        doublet
+    }
 }
 
 fn main() -> Result<(), failure::Error> {
@@ -82,6 +120,19 @@ fn main() -> Result<(), failure::Error> {
 
     debug!("Using input {}-{}", start, end);
 
+    let (mut pws, mut eps) = (0, 0);
+    for n in start..=end {
+        let p = Password::from_number(n);
+        if p.valid() {
+            pws += 1;
+        }
+        if p.extra_valid() {
+            eps += 1;
+        }
+    }
+
+    println!("Found {} or {} valid passwords", pws, eps);
+
     Ok(())
 }
 
@@ -106,5 +157,29 @@ mod tests {
         assert!(Password::from_number(111_111).valid());
         assert!(!Password::from_number(223_450).valid());
         assert!(!Password::from_number(123_789).valid());
+    }
+
+    #[test]
+    fn test_more_validity() {
+        // Decreasing
+        assert!(!Password::from_number(124_435).extra_valid());
+        // No repeat
+        assert!(!Password::from_number(124_589).extra_valid());
+        // Repeat but no doublet
+        assert!(!Password::from_number(124_449).extra_valid());
+
+        // valid
+        assert!(Password::from_number(122_345).extra_valid());
+        assert!(Password::from_number(135_669).extra_valid());
+
+        // From assignment part 1
+        assert!(!Password::from_number(111_111).extra_valid());
+        assert!(!Password::from_number(223_450).extra_valid());
+        assert!(!Password::from_number(123_789).extra_valid());
+
+        // From assignment part 2
+        assert!(Password::from_number(112_233).extra_valid());
+        assert!(!Password::from_number(123_444).extra_valid());
+        assert!(Password::from_number(111_122).extra_valid());
     }
 }
