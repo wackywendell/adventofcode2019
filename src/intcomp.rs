280f248d42a022383595fdaab4bdd4961a17fc99
Wendell Smith <wackywendell@gmail.com>
2020-02-02 18:47:01 -0500

    Passes more part two tests, with a bugfix and more logging

--- src/intcomp.rs
+++ src/intcomp.rs
@@ -15,6 +15,10 @@ pub enum Code {
     Multiply,
     Input,
     Output,
+    TrueJump,
+    FalseJump,
+    LessThan,
+    Equals,
     Halt,
 }
 
@@ -25,6 +29,10 @@ impl Code {
             Code::Multiply => 3,
             Code::Input => 1,
             Code::Output => 1,
+            Code::TrueJump => 2,
+            Code::FalseJump => 2,
+            Code::LessThan => 3,
+            Code::Equals => 3,
             Code::Halt => 0,
         }
     }
@@ -37,6 +45,10 @@ impl fmt::Display for Code {
             Code::Multiply => "Mul",
             Code::Input => "In",
             Code::Output => "Out",
+            Code::TrueJump => "TJmp",
+            Code::FalseJump => "FJmp",
+            Code::LessThan => "Less",
+            Code::Equals => "Eq",
             Code::Halt => "Halt",
         };
 
@@ -71,6 +83,10 @@ impl TryFrom<Value> for Code {
             2 => Ok(Code::Multiply),
             3 => Ok(Code::Input),
             4 => Ok(Code::Output),
+            5 => Ok(Code::TrueJump),
+            6 => Ok(Code::FalseJump),
+            7 => Ok(Code::LessThan),
+            8 => Ok(Code::Equals),
             99 => Ok(Code::Halt),
             v => Err(CodeFindError { value: v }),
         }
@@ -81,6 +97,15 @@ impl TryFrom<Value> for Code {
 pub enum Mode {
     Position,
     Immediate,
+}
+
+impl fmt::Display for Mode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Mode::Position => write!(f, "Mode::Position"),
+            Mode::Immediate => write!(f, "Mode::Immediate"),
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
@@ -107,6 +132,15 @@ impl TryFrom<Value> for Instruction {
         let mode2 = mode_from_num((value / 1000) % 10).ok_or(CodeFindError { value })?;
         let mode3 = mode_from_num((value / 10_000) % 10).ok_or(CodeFindError { value })?;
 
+        log::debug!(
+            "Parsed: {} -> Code {} -> {},{},{}",
+            value,
+            code,
+            mode1,
+            mode2,
+            mode3,
+        );
+
         Ok(Instruction {
             code,
             modes: [mode1, mode2, mode3],
@@ -120,11 +154,19 @@ impl Instruction {
         let mut params = Vec::with_capacity(rn);
         for j in 0..rn {
             let value = registers[ix + j + 1];
-            let value = match self.modes[j] {
+            let new_value = match self.modes[j] {
                 Mode::Immediate => value,
                 Mode::Position => registers[value as usize],
             };
-            params.push(value);
+            log::debug!(
+                "    Parameter {} ({}): {} ({}) -> {}",
+                j,
+                ix + j + 1,
+                value,
+                self.modes[j],
+                new_value
+            );
+            params.push(new_value);
         }
 
         params
@@ -213,7 +255,7 @@ impl IntComp {
         );
         log::debug!("  Parameters: {:?}", params);
 
-        let adv = match instruction.code {
+        self.position = match instruction.code {
             Code::Add => {
                 let out_ix = self.get(pos + 3)? as usize;
                 let out_loc = self.get_mut(out_ix)?;
@@ -224,7 +266,7 @@ impl IntComp {
                     params[0] + params[1],
                 );
                 *out_loc = params[0] + params[1];
-                Some(4)
+                Some(pos + 4)
             }
             Code::Multiply => {
                 let out_ix = self.get(pos + 3)? as usize;
@@ -237,31 +279,90 @@ impl IntComp {
                 );
 
                 *out_loc = params[0] * params[1];
-                Some(4)
+                Some(pos + 4)
             }
             Code::Input => {
                 let input = self
                     .inputs
                     .pop_front()
                     .ok_or(InvalidInstruction::MissingInput { position: pos })?;
-                let out_ix = self.get(pos + 3)? as usize;
+                let out_ix = self.get(pos + 1)? as usize;
                 let out_loc = self.get_mut(out_ix)?;
+                log::debug!("  Input {}: {} -> {}", out_ix, *out_loc, input,);
                 *out_loc = input;
-                Some(2)
+                Some(pos + 2)
             }
             Code::Output => {
                 self.outputs.push_back(params[0]);
-                Some(2)
+                log::debug!("  Output {}", params[0]);
+                Some(pos + 2)
+            }
+            Code::TrueJump => {
+                if params[0] != 0 {
+                    log::debug!(
+                        "  TrueJump Jump: {} != 0: Jump -> {}",
+                        params[0],
+                        params[1] as usize
+                    );
+                    Some(params[1] as usize)
+                } else {
+                    log::debug!(
+                        "  TrueJump Advance: {} == 0: Jump -> {}",
+                        params[0],
+                        pos + 2,
+                    );
+                    Some(pos + 2)
+                }
+            }
+            Code::FalseJump => {
+                if params[0] == 0 {
+                    log::debug!(
+                        "  TrueJump Jump: {} == 0: Jump -> {}",
+                        params[0],
+                        params[1] as usize
+                    );
+                    Some(params[1] as usize)
+                } else {
+                    log::debug!(
+                        "  TrueJump Advance: {} != 0: Jump -> {}",
+                        params[0],
+                        pos + 2,
+                    );
+                    Some(pos + 2)
+                }
+            }
+            Code::LessThan => {
+                let out_ix = self.get(pos + 3)? as usize;
+                let out_loc = self.get_mut(out_ix)?;
+                let out = if params[0] < params[1] { 1 } else { 0 };
+                log::debug!(
+                    "  LessThan: {} < {}: {} -> {}",
+                    params[0],
+                    params[1],
+                    *out_loc,
+                    out,
+                );
+                *out_loc = out;
+                Some(pos + 4)
+            }
+            Code::Equals => {
+                let out_ix = self.get(pos + 3)? as usize;
+                let out_loc = self.get_mut(out_ix)?;
+                let out = if params[0] == params[1] { 1 } else { 0 };
+                log::debug!(
+                    "  Equals: {} == {}: {} -> {}",
+                    params[0],
+                    params[1],
+                    *out_loc,
+                    out,
+                );
+                *out_loc = out;
+                Some(pos + 4)
             }
             Code::Halt => None,
         };
 
-        self.position = match adv {
-            None => None,
-            Some(a) => Some(pos + a),
-        };
-
-        Ok(adv.is_some())
+        Ok(self.position.is_some())
     }
 
     pub fn run(&mut self) -> Result<(), failure::Error> {
@@ -365,6 +466,111 @@ mod tests {
         assert_eq!(cp.position, None);
         assert_eq!(cp.values, vec![1002, 4, 3, 4, 99]);
 
+        Ok(())
+    }
+
+    #[test]
+    fn test_equals() -> Result<(), failure::Error> {
+        log::debug!("-- Test 1 --");
+        let orig_cp = IntComp::from_str("3,9,8,9,10,9,4,9,99,-1,8")?;
+        let mut cp = orig_cp.clone();
+        cp.inputs.push_back(8);
+        cp.run()?;
+        assert_eq!(cp.outputs.pop_front(), Some(1));
+
+        log::debug!("-- Test 2 --");
+        let mut cp = orig_cp.clone();
+        cp.inputs.push_back(3);
+        cp.run()?;
+        assert_eq!(cp.outputs.pop_front(), Some(0));
+
+        log::debug!("-- Test 3 --");
+        let mut cp = orig_cp;
+        cp.inputs.push_back(2);
+        cp.run()?;
+        assert_eq!(cp.outputs.pop_front(), Some(0));
+
+        log::debug!("-- Test 4 --");
+        let orig_cp = IntComp::from_str("3,3,1108,-1,8,3,4,3,99")?;
+        let mut cp = orig_cp.clone();
+        cp.inputs.push_back(8);
+        cp.run()?;
+        assert_eq!(cp.outputs.pop_front(), Some(1));
+
+        log::debug!("-- Test 5 --");
+        let mut cp = orig_cp.clone();
+        cp.inputs.push_back(3);
+        cp.run()?;
+        assert_eq!(cp.outputs.pop_front(), Some(0));
+
+        log::debug!("-- Test 6 --");
+        let mut cp = orig_cp;
+        cp.inputs.push_back(2);
+        cp.run()?;
+        assert_eq!(cp.outputs.pop_front(), Some(0));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_less() -> Result<(), failure::Error> {
+        let orig_cp = IntComp::from_str("3,9,7,9,10,9,4,9,99,-1,8")?;
+        let mut cp = orig_cp.clone();
+        cp.inputs.push_back(3);
+        cp.run()?;
+        assert_eq!(cp.outputs.pop_front(), Some(1));
+
+        let mut cp = orig_cp.clone();
+        cp.inputs.push_back(8);
+        cp.run()?;
+        assert_eq!(cp.outputs.pop_front(), Some(0));
+
+        let mut cp = orig_cp;
+        cp.inputs.push_back(20);
+        cp.run()?;
+        assert_eq!(cp.outputs.pop_front(), Some(0));
+
+        let orig_cp = IntComp::from_str("3,3,1107,-1,8,3,4,3,99")?;
+        let mut cp = orig_cp.clone();
+        cp.inputs.push_back(3);
+        cp.run()?;
+        assert_eq!(cp.outputs.pop_front(), Some(1));
+
+        let mut cp = orig_cp.clone();
+        cp.inputs.push_back(8);
+        cp.run()?;
+        assert_eq!(cp.outputs.pop_front(), Some(0));
+
+        let mut cp = orig_cp;
+        cp.inputs.push_back(20);
+        cp.run()?;
+        assert_eq!(cp.outputs.pop_front(), Some(0));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_jump() -> Result<(), failure::Error> {
+        let cps = [
+            IntComp::from_str("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9")?,
+            IntComp::from_str("3,3,1105,-1,9,1101,0,0,12,4,12,99,1")?,
+        ];
+        for ocp in &cps {
+            let mut cp = ocp.clone();
+            cp.inputs.push_back(0);
+            cp.run()?;
+            assert_eq!(cp.outputs.pop_front(), Some(0));
+
+            let mut cp = ocp.clone();
+            cp.inputs.push_back(8);
+            cp.run()?;
+            assert_eq!(cp.outputs.pop_front(), Some(1));
+
+            let mut cp = ocp.clone();
+            cp.inputs.push_back(-20);
+            cp.run()?;
+            assert_eq!(cp.outputs.pop_front(), Some(1));
+        }
         Ok(())
     }
 }
