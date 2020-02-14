2b59eb265bce8b55b4954e5b4a118d5557e8a754
Wendell Smith <wackywendell@gmail.com>
2020-02-14 17:35:41 -0500

    A bit more on day 8

--- src/day8/main.rs
+++ src/day8/main.rs
@@ -5,6 +5,58 @@ use std::io::BufReader;
 use clap::{App, Arg};
 use log::debug;
 
+pub struct Image {
+    pixels: Vec<Vec<Vec<u8>>>,
+}
+
+impl Image {
+    pub fn new(
+        input: Vec<u8>,
+        layers: usize,
+        width: usize,
+        height: usize,
+    ) -> Result<Image, failure::Error> {
+        let size = width * height;
+        if input.len() != layers * size {
+            return Err(failure::err_msg(format!(
+                "Found {} pixels; expected {}*{}*{}={}",
+                input.len(),
+                layers,
+                height,
+                width,
+                layers * size
+            )));
+        }
+
+        let mut pixels: Vec<Vec<Vec<u8>>> = Vec::with_capacity(layers);
+        for layer in input.chunks(size) {
+            let mut rows: Vec<Vec<u8>> = Vec::with_capacity(height);
+            for row in layer.chunks(width) {
+                rows.push(row.to_owned())
+            }
+            pixels.push(rows);
+        }
+
+        Ok(Image { pixels })
+    }
+}
+
+fn parse_digits(line: &str) -> Result<Vec<u8>, failure::Error> {
+    let mut ints = Vec::new();
+
+    for c in line.chars() {
+        let n = match c.to_digit(10) {
+            None => {
+                return Err(failure::err_msg(format!("Expected digit, found '{}'", c)));
+            }
+            Some(n) => n,
+        };
+        ints.push(n as u8);
+    }
+
+    Ok(ints)
+}
+
 fn main() -> Result<(), failure::Error> {
     env_logger::init();
 
@@ -24,9 +76,15 @@ fn main() -> Result<(), failure::Error> {
     let file = File::open(input_path)?;
     let buf_reader = BufReader::new(file);
 
+    let mut ints: Vec<u8> = Vec::new();
+
     for line in buf_reader.lines() {
-        println!("{}", line?)
+        let line = line?;
+        let mut v = parse_digits(line.trim())?;
+        ints.append(&mut v);
     }
+
+    println!("Found {} ints", ints.len());
 
     Ok(())
 }
