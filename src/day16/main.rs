6075ecc106a2bdc91eacd1095100f6a37f6ce17e
Wendell Smith <wackywendell@gmail.com>
2020-06-28 22:22:11 -0400

    Part one works

--- src/day16/main.rs
+++ src/day16/main.rs
@@ -1,9 +1,41 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::str::FromStr;
 
 use clap::{App, Arg};
 use log::debug;
+
+#[derive(Clone, PartialEq, PartialOrd, Eq, Ord, Debug)]
+pub struct Signal {
+    values: Vec<i64>,
+}
+
+impl Signal {
+    pub fn new(values: Vec<i64>) -> Signal {
+        Signal { values }
+    }
+}
+
+impl FromStr for Signal {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let values: anyhow::Result<Vec<i64>> = s
+            .chars()
+            .map(|c| {
+                c.to_digit(10)
+                    .map(|n| n as i64)
+                    .ok_or_else(|| anyhow::format_err!("Cannot parse character {} as int", c))
+            })
+            .collect();
+
+        Ok(Signal::new(values?))
+    }
+}
+
+fn ones_digit(n: i64) -> i64 {
+    (n % 10).abs()
+}
 
 #[derive(Copy, Clone)]
 pub struct BasicPattern {
@@ -16,6 +48,35 @@ impl BasicPattern {
             panic!("Form {} must be >= 1", form);
         }
         BasicPattern { form }
+    }
+
+    pub fn apply(self, signal: &Signal) -> i64 {
+        ones_digit(
+            signal
+                .values
+                .iter()
+                .zip(self.into_iter())
+                .map(|(s, v)| s * v)
+                .sum::<i64>(),
+        )
+    }
+
+    pub fn transform(signal: &Signal) -> Signal {
+        let len = signal.values.len();
+        Signal::new(
+            (1..=len)
+                .map(|n| BasicPattern::new(n as i64).apply(signal))
+                .collect(),
+        )
+    }
+
+    pub fn repeated_transform(signal: &Signal, n: isize) -> Signal {
+        let mut signal: Signal = signal.clone();
+        for _ in 0..n {
+            signal = BasicPattern::transform(&signal);
+        }
+
+        signal
     }
 }
 
@@ -44,10 +105,6 @@ impl Iterator for BasicPatternIter {
         let ix = ((self.index + 1) / self.form) % 4;
         self.index += 1;
         let values = [0i64, 1, 0, -1];
-        println!(
-            "ix: {}, self.index: {}, self.form: {}",
-            ix, self.index, self.form
-        );
         Some(values[ix as usize])
     }
 }
@@ -71,8 +128,14 @@ fn main() -> anyhow::Result<()> {
     let file = File::open(input_path)?;
     let buf_reader = BufReader::new(file);
 
-    for line in buf_reader.lines() {
-        println!("{}", line?)
+    for maybe_line in buf_reader.lines() {
+        let line = maybe_line?;
+        let signal: Signal = line.parse()?;
+        let output = BasicPattern::repeated_transform(&signal, 100);
+        for n in &output.values[..8] {
+            print!("{}", n);
+        }
+        println!();
     }
 
     Ok(())
@@ -97,6 +160,57 @@ mod tests {
         let expected = vec![0, 1, 1, 0, 0, -1, -1, 0, 0, 1, 1, 0, 0, -1, -1, 0, 0];
         let values: Vec<i64> = iter.take(expected.len()).collect();
         assert_eq!(values, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_pattern_apply() -> anyhow::Result<()> {
+        let signal: Signal = str::parse("12345678")?;
+        assert_eq!(BasicPattern::new(1).apply(&signal), 4);
+        assert_eq!(BasicPattern::new(2).apply(&signal), 8);
+        assert_eq!(BasicPattern::new(3).apply(&signal), 2);
+        assert_eq!(BasicPattern::new(4).apply(&signal), 2);
+        assert_eq!(BasicPattern::new(5).apply(&signal), 6);
+        assert_eq!(BasicPattern::new(6).apply(&signal), 1);
+        assert_eq!(BasicPattern::new(7).apply(&signal), 5);
+        assert_eq!(BasicPattern::new(8).apply(&signal), 8);
+
+        let outputs = [
+            str::parse("12345678")?,
+            str::parse("48226158")?,
+            str::parse("34040438")?,
+            str::parse("03415518")?,
+            str::parse("01029498")?,
+        ];
+
+        for (n, row) in outputs.iter().enumerate() {
+            let mut found_output: Signal = signal.clone();
+            for _ in 0..n {
+                found_output = BasicPattern::transform(&found_output)
+            }
+            assert_eq!(&found_output, row);
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_simple_applies() -> anyhow::Result<()> {
+        let signal: Signal = str::parse("80871224585914546619083218645595")?;
+        let output = BasicPattern::repeated_transform(&signal, 100);
+        let expected: Signal = str::parse("24176176")?;
+        assert_eq!(output.values[..8], expected.values[..]);
+
+        let signal: Signal = str::parse("19617804207202209144916044189917")?;
+        let output = BasicPattern::repeated_transform(&signal, 100);
+        let expected: Signal = str::parse("73745418")?;
+        assert_eq!(output.values[..8], expected.values[..]);
+
+        let signal: Signal = str::parse("69317163492948606335995924319873")?;
+        let output = BasicPattern::repeated_transform(&signal, 100);
+        let expected: Signal = str::parse("52432133")?;
+        assert_eq!(output.values[..8], expected.values[..]);
 
         Ok(())
     }
