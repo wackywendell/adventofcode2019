fe583e18985d285e459333c451732ed6bcfaa970
Wendell Smith <wackywendell@gmail.com>
2020-06-28 20:33:45 -0400

    Started day 16

---
+++ src/day16/main.rs
@@ -0,0 +1,44 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+use clap::{App, Arg};
+use log::debug;
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let matches = App::new("Day 16")
+        .arg(
+            Arg::with_name("input")
+                .short("i")
+                .long("input")
+                .value_name("INPUT")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let input_path = matches.value_of("INPUT").unwrap_or("inputs/day16.txt");
+
+    debug!("Using input {}", input_path);
+    let file = File::open(input_path)?;
+    let buf_reader = BufReader::new(file);
+
+    for line in buf_reader.lines() {
+        println!("{}", line?)
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use test_env_log::test;
+
+    // use super::*;
+
+    #[test]
+    fn test_thing() -> anyhow::Result<()> {
+        Ok(())
+    }
+}
