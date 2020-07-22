a092ea8eb2170047f4d3e28cefe83eea8d17dd0d
Wendell Smith <wackywendell@gmail.com>
2020-07-22 11:26:41 -0400

    Create grid, map structures

--- src/grid.rs
+++ src/grid.rs
@@ -1,14 +1,12 @@
-use std::collections::{hash_map::Entry, HashMap, HashSet};
-use std::convert::TryFrom;
+use std::collections::{HashMap, HashSet};
 use std::fmt;
 use std::hash::Hash;
 use std::ops;
-use std::str::FromStr;
 
 type Value = i64;
 
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, Default)]
-pub struct Position(Value, Value);
+pub struct Position(pub Value, pub Value);
 
 impl fmt::Display for Position {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -89,28 +87,67 @@ impl fmt::Display for Turn {
     }
 }
 
-pub struct Grid<T> {
+pub enum Token<T> {
+    Skip,
+    NewRow,
+    Item(T),
+}
+
+pub trait FromSequence<C>: Sized {
+    type Error;
+
+    fn to_token(c: C) -> Result<Token<Self>, Self::Error>;
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Map<T> {
     pub shape: (Value, Value),
     pub grid: HashMap<Position, T>,
 }
 
-impl<T: TryFrom<char>> FromStr for Grid<T>
-where
-    <T as TryFrom<char>>::Error: std::error::Error + Sync + Send + 'static,
-{
-    type Err = anyhow::Error;
+impl<T> Default for Map<T> {
+    // Derived default requires T: Default; this manual impl does not
+    fn default() -> Self {
+        Map {
+            shape: Default::default(),
+            grid: Default::default(),
+        }
+    }
+}
 
-    fn from_str(s: &str) -> anyhow::Result<Grid<T>> {
+impl<T: Eq> Map<T> {
+    pub fn get(&self, pos: Position) -> Option<&T> {
+        self.grid.get(&pos)
+    }
+    pub fn insert(&mut self, pos: Position, item: T) -> Option<T> {
+        let Position(x, y) = pos;
+        if x > self.shape.0 {
+            self.shape = (x, self.shape.1);
+        }
+        if y > self.shape.1 {
+            self.shape = (self.shape.0, y);
+        }
+        self.grid.insert(pos, item)
+    }
+}
+
+impl<T> std::iter::FromIterator<Token<T>> for Map<T>
+where
+    T: Hash + Eq + fmt::Debug,
+    // <T as FromSequence<C>>::Error: std::error::Error + Sync + Send + 'static,
+{
+    // type Error = anyhow::Error;
+    fn from_iter<I: IntoIterator<Item = Token<T>>>(iter: I) -> Self {
         let mut maxx = 0;
         let mut maxy = 0;
         let (mut x, mut y) = (0i64, 0i64);
-
         let mut grid = HashMap::new();
 
-        for c in s.chars() {
-            match c {
-                ' ' => continue,
-                '\n' => {
+        for token in iter {
+            let pos = Position(x, y);
+            match token {
+                Token::Skip => continue,
+                Token::NewRow => {
                     if x == 0 {
                         continue;
                     }
@@ -119,9 +156,8 @@ where
                     x = 0;
                     continue;
                 }
-                c => {
-                    let place = T::try_from(c)?;
-                    grid.insert(Position(x, y), place);
+                Token::Item(item) => {
+                    grid.insert(pos, item);
                 }
             }
 
@@ -132,107 +168,92 @@ where
             x += 1;
         }
 
-        Ok(Grid {
+        Map {
             shape: (maxx, maxy),
             grid,
-        })
+        }
     }
 }
+
+// impl<T> FromStr for Map<T>
+// where
+//     T: Hash + Eq + fmt::Debug,
+//     T: TryFrom<(char, Position)>,
+//     <T as TryFrom<(char, Position)>>::Error: std::error::Error + Sync + Send + 'static,
+// {
+//     type Err = anyhow::Error;
+
+//     fn from_str(s: &str) -> anyhow::Result<Map<T>> {
+//         let mut maxx = 0;
+//         let mut maxy = 0;
+//         let (mut x, mut y) = (0i64, 0i64);
+//         let mut grid = HashMap::new();
+
+//         for c in s.chars() {
+//             let pos = Position(x, y);
+//             match c {
+//                 ' ' => continue,
+//                 '\n' => {
+//                     if x == 0 {
+//                         continue;
+//                     }
+//                     maxy = y;
+//                     y += 1;
+//                     x = 0;
+//                     continue;
+//                 }
+//                 c => {
+//                     let square: T = TryFrom::try_from((c, pos))?;
+//                     grid.insert(pos, square);
+//                 }
+//             }
+
+//             if x > maxx {
+//                 maxx = x;
+//             }
+
+//             x += 1;
+//         }
+
+//         Ok(Map {
+//             shape: (maxx, maxy),
+//             grid,
+//         })
+//     }
+// }
 
 pub enum Structure {
     Passable,
     Blocked,
 }
 
-pub enum Square<Special> {
-    Passable,
-    Blocked,
-    PassableSpecial(Special),
-    BlockedSpecial(Special),
-}
-
-pub struct Map<T: Hash + Eq> {
+pub struct Grid {
     pub shape: (Value, Value),
     pub grid: HashSet<Position>,
-    pub specials: HashMap<T, Position>,
 }
 
-impl<T: Hash + Eq + fmt::Debug> Map<T> {
-    fn insert_special(&mut self, s: T, pos: Position) -> anyhow::Result<()> {
-        match self.specials.entry(s) {
-            Entry::Occupied(e) => Err(anyhow::format_err!(
-                "Found {:?} at {} and {}",
-                e.key(),
-                e.get(),
-                pos
-            )),
-            Entry::Vacant(e) => {
-                e.insert(pos);
-                Ok(())
-            }
-        }
-    }
+pub trait SquareAttributes {
+    fn is_passable(&self) -> bool;
+    fn is_item(&self) -> bool;
 }
 
-impl<T> FromStr for Map<T>
-where
-    T: Hash + Eq + fmt::Debug,
-    Square<T>: TryFrom<(char, Position)>,
-    <Square<T> as TryFrom<(char, Position)>>::Error: std::error::Error + Sync + Send + 'static,
-{
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> anyhow::Result<Map<T>> {
-        let mut maxx = 0;
-        let mut maxy = 0;
-        let (mut x, mut y) = (0i64, 0i64);
-
-        let mut map = Map {
-            shape: (0, 0),
+impl<T: Eq + Hash + SquareAttributes> From<Map<T>> for (Grid, Vec<(T, Position)>) {
+    fn from(map: Map<T>) -> Self {
+        let mut grid = Grid {
+            shape: map.shape,
             grid: HashSet::new(),
-            specials: HashMap::new(),
         };
+        let mut items = Vec::new();
 
-        for c in s.chars() {
-            let pos = Position(x, y);
-            match c {
-                ' ' => continue,
-                '\n' => {
-                    if x == 0 {
-                        continue;
-                    }
-                    maxy = y;
-                    y += 1;
-                    x = 0;
-                    continue;
-                }
-                c => {
-                    let place: Square<T> = TryFrom::try_from((c, pos))?;
-                    match place {
-                        Square::Blocked => continue,
-                        Square::Passable => {
-                            map.grid.insert(pos);
-                        }
-                        Square::BlockedSpecial(s) => {
-                            map.insert_special(s, pos)?;
-                        }
-                        Square::PassableSpecial(s) => {
-                            map.grid.insert(pos);
-                            map.insert_special(s, pos)?;
-                        }
-                    }
-                }
+        for (pos, square) in map.grid {
+            if square.is_passable() {
+                grid.grid.insert(pos);
             }
-
-            if x > maxx {
-                maxx = x;
+            if square.is_item() {
+                items.push((square, pos));
             }
-
-            x += 1;
         }
 
-        map.shape = (maxx, maxy);
-
-        Ok(map)
+        (grid, items)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+pub mod grid;
 pub mod intcomp;
 pub mod parse;
