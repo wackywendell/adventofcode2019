9dc44eba6f15a6f15db574f83e9a4a2a7aa2abcb
Wendell Smith <wackywendell@gmail.com>
2021-04-29 16:48:29 -0400

    Finished Day 21

--- src/day21/main.rs
+++ src/day21/main.rs
@@ -5,6 +5,43 @@ use std::io::BufReader;
 use clap::{App, Arg};
 use log::debug;
 
+use aoc::intcomp::{IntComp, OutputVec, Stopped};
+
+const INTO_HOLE_PROGRAM: &str = "\
+NOT D J
+WALK
+";
+
+/*
+((!A || !B || !C) && D)
+*/
+const JUMP_PROGRAM1: &str = "\
+NOT A J
+NOT B T
+OR T J
+NOT C T
+OR T J
+AND D J
+WALK
+";
+
+/*
+((!A || (!B && ) || !C) && D)
+*/
+const JUMP_PROGRAM2: &str = "\
+NOT A J
+NOT B T
+OR T J
+NOT C T
+OR T J
+AND D J
+NOT E T
+NOT T T
+OR H T
+AND T J
+RUN
+";
+
 fn main() -> anyhow::Result<()> {
     env_logger::init();
 
@@ -24,8 +61,47 @@ fn main() -> anyhow::Result<()> {
     let file = File::open(input_path)?;
     let buf_reader = BufReader::new(file);
 
-    for line in buf_reader.lines() {
-        println!("{}", line?)
+    let line: String = buf_reader
+        .lines()
+        .next()
+        .ok_or_else(|| anyhow::format_err!("No line found"))??;
+
+    let mut cp: IntComp = str::parse(&line)?;
+    let input: Vec<i64> = JUMP_PROGRAM2.as_bytes().iter().map(|&n| n as i64).collect();
+
+    let mut outputs = OutputVec::new();
+    cp.run_to_input(&mut outputs)?.expect(Stopped::Input)?;
+    let output_bytes: Vec<u8> = outputs.0.iter().map(|&n| n as u8).collect();
+    let output_str = std::str::from_utf8(&output_bytes)?;
+    println!("-> {}", output_str);
+    outputs.0.clear();
+    cp.process(input, &mut outputs)?.expect(Stopped::Halted)?;
+
+    println!("Output length: {}", outputs.0.len());
+    let output_vec: Vec<i64> = outputs.0.iter().copied().collect();
+    println!("Output: {:?}", output_vec);
+
+    let output_bytes: Vec<u8> = outputs
+        .0
+        .iter()
+        .map(|&n| if n <= 255 { n as u8 } else { 255 })
+        .collect();
+
+    match std::str::from_utf8(&output_bytes) {
+        Ok(output_str) => {
+            println!("-> ...\n{}", output_str);
+            return Ok(());
+        }
+        Err(e) => {
+            let n = e.valid_up_to();
+            let output_str = std::str::from_utf8(&output_bytes[..n])?;
+            println!("-> {}", output_str);
+            let back: i64 = *(outputs
+                .0
+                .back()
+                .ok_or_else(|| anyhow::anyhow!("Expected non-empty output"))?);
+            println!("-> {}", back);
+        }
     }
 
     Ok(())
