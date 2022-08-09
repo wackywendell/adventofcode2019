174359297b97e732c10f0c4110c113fceaea5825
Wendell Smith <wackywendell@gmail.com>
2022-08-09 19:08:34 -0400

    Day 25 clippy

--- src/day25/main.rs
+++ src/day25/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
+use std::collections::{BTreeMap, BTreeSet, HashMap};
 use std::fmt::{Display, Formatter, Result as FmtResult};
 use std::fs::File;
 use std::io::prelude::*;
